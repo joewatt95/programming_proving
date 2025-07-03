cf7a5ce662819be446db154e09f2fc3d547492f1
joewatt95 <joewatt95@u.nus.edu>
2025-07-03 15:19:11 +0800

    tidy

--- isabelle/Tree.thy
+++ isabelle/Tree.thy
@@ -19,18 +19,14 @@ datatype 'a tree =
 
 abbreviation \<open>Leaf \<equiv> flip (Node Tip) Tip\<close>
 
-definition tree_to_set :: \<open>'a tree \<Rightarrow> 'a set\<close> where
-  \<open>tree_to_set \<equiv> rec_tree \<emptyset> (\<lambda> _ item' _. insert item' >>> (\<union>))\<close>
-
-(* abbreviation \<open>min_tree \<equiv> Min <<< tree_to_set\<close>
-abbreviation \<open>max_tree \<equiv> Max <<< tree_to_set\<close> *)
+abbreviation \<open>tree_to_set \<equiv> rec_tree \<emptyset> (\<lambda> _ item' _. insert item' >>> (\<union>))\<close>
 
 definition tree_ordered :: \<open>'a::{linorder} tree \<Rightarrow> bool\<close> where
   \<open>tree_ordered \<equiv> rec_tree True (
     \<lambda> left' item' right' left_ordered right_ordered.
-      left_ordered \<and> right_ordered \<and> (
-      let cmp = \<lambda> x ord tree. tree = Tip \<or> ord x (item tree)
-      in cmp item' (\<ge>) left' \<and> cmp item' (\<le>) right'))\<close>
+      left_ordered \<and> right_ordered \<and>
+      (\<forall> x \<in> tree_to_set left'. x \<le> item') \<and>
+      (\<forall> x \<in> tree_to_set right'. x \<ge> item'))\<close>
 
 fun ins :: \<open>'a::{linorder} \<Rightarrow> 'a tree \<Rightarrow> 'a tree\<close> where
   \<open>ins x Tip = Leaf x\<close> |
@@ -39,21 +35,13 @@ fun ins :: \<open>'a::{linorder} \<Rightarrow> 'a tree \<Rightarrow> 'a tree\<cl
     else if x < val' then Node (ins x left') val' right'
     else Node left' val' (ins x right'))\<close>
 
-lemma item_ins_eq:
-  \<open>item (ins x tree) = (if tree = Tip then x else item tree)\<close>
-  by (cases tree) auto
-
-(* lemma ins_ne_tip [simp]:
-  \<open>ins x tree \<noteq> Tip\<close>
-  by (cases tree) auto *)
-
-lemma ins_correct_1:
+lemma tree_to_set_ins_eq_insert_tree_to_set [simp]:
   \<open>tree_to_set (ins x tree) = insert x (tree_to_set tree)\<close>
-  by (induction tree) (auto simp add: tree_to_set_def)
+  by (induction tree) auto
 
-lemma ins_correct_2:
+lemma tree_ordered_ins_of_ordered:
   \<open>tree_ordered tree \<Longrightarrow> tree_ordered (ins x tree)\<close>
-  by (induction tree) (auto simp add: tree_ordered_def item_ins_eq)
+  by (induction tree) (auto simp add: tree_ordered_def)
 
 end
 
