b7db1e3999b6ecbb661073fef899b90c611484ce
joewatt95 <joewatt95@u.nus.edu>
2025-07-03 14:40:42 +0800

    tidy

--- isabelle/Tree.thy
+++ isabelle/Tree.thy
@@ -17,30 +17,20 @@ datatype 'a tree =
   Tip |
   Node (left: \<open>'a tree\<close>) (item: 'a) (right: \<open>'a tree\<close>)
 
-lemma tree_ne_Tip_iff_eq_Node:
-  \<open>tree \<noteq> Tip \<longleftrightarrow> (\<exists> left' item' right'. tree = Node left' item' right')\<close>
-  by (blast intro: tree.exhaust_sel)
-
-lemmas tree_simps = tree.case_eq_if tree_ne_Tip_iff_eq_Node
-
 abbreviation \<open>Leaf \<equiv> flip (Node Tip) Tip\<close>
 
-abbreviation
-  \<open>tree_to_set \<equiv>
-    rec_tree \<emptyset> (\<lambda> _ item' _ left_set. insert item' <<< (\<union>) left_set)\<close>
+definition tree_to_set :: \<open>'a tree \<Rightarrow> 'a set\<close> where
+  \<open>tree_to_set \<equiv> rec_tree \<emptyset> (\<lambda> _ item' _. insert item' >>> (\<union>))\<close>
 
 (* abbreviation \<open>min_tree \<equiv> Min <<< tree_to_set\<close>
 abbreviation \<open>max_tree \<equiv> Max <<< tree_to_set\<close> *)
 
-fun tree_ordered :: \<open>'a::{linorder} tree \<Rightarrow> bool\<close> where
-  \<open>tree_ordered Tip = True\<close> |
-  \<open>tree_ordered (Node left' item' right') = (
-    tree_ordered left' \<and>
-    tree_ordered right' \<and> (
-    let cmp = \<lambda> x ord tree. case tree of
-      Tip \<Rightarrow> True |
-      Node _ item' _ \<Rightarrow> ord x item'
-    in cmp item' (\<ge>) left' \<and> cmp item' (\<le>) right'))\<close>
+definition tree_ordered :: \<open>'a::{linorder} tree \<Rightarrow> bool\<close> where
+  \<open>tree_ordered \<equiv> rec_tree True (
+    \<lambda> left' item' right' left_ordered right_ordered.
+      left_ordered \<and> right_ordered \<and> (
+      let cmp = \<lambda> x ord tree. tree = Tip \<or> ord x (item tree)
+      in cmp item' (\<ge>) left' \<and> cmp item' (\<le>) right'))\<close>
 
 fun ins :: \<open>'a::{linorder} \<Rightarrow> 'a tree \<Rightarrow> 'a tree\<close> where
   \<open>ins x Tip = Leaf x\<close> |
@@ -49,16 +39,21 @@ fun ins :: \<open>'a::{linorder} \<Rightarrow> 'a tree \<Rightarrow> 'a tree\<cl
     else if x < val' then Node (ins x left') val' right'
     else Node left' val' (ins x right'))\<close>
 
+lemma item_ins_eq:
+  \<open>item (ins x tree) = (if tree = Tip then x else item tree)\<close>
+  by (cases tree) auto
+
+(* lemma ins_ne_tip [simp]:
+  \<open>ins x tree \<noteq> Tip\<close>
+  by (cases tree) auto *)
+
 lemma ins_correct_1:
   \<open>tree_to_set (ins x tree) = insert x (tree_to_set tree)\<close>
-  apply (induction tree) by auto
+  by (induction tree) (auto simp add: tree_to_set_def)
 
 lemma ins_correct_2:
-  assumes \<open>tree_ordered tree\<close>
-  shows \<open>tree_ordered <| ins x tree\<close>
- using assms
- apply (induction tree)
- by (auto simp add: tree_simps elim!: ins.elims)
+  \<open>tree_ordered tree \<Longrightarrow> tree_ordered (ins x tree)\<close>
+  by (induction tree) (auto simp add: tree_ordered_def item_ins_eq)
 
 end
 
