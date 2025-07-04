8a58f153e06ba433ee38c573d21fc710533b345c
Joe Watt <joewatt95@u.nus.edu>
2025-07-04 22:25:25 +0800

    tidy

--- isabelle/Tree.thy
+++ isabelle/Tree.thy
@@ -21,14 +21,14 @@ abbreviation \<open>Leaf \<equiv> flip (Node Tip) Tip\<close>
 
 abbreviation \<open>tree_to_set \<equiv> rec_tree \<emptyset> (\<lambda> _ item' _. insert item' >>> (\<union>))\<close>
 
-definition ord :: \<open>'a::{linorder} tree \<Rightarrow> bool\<close> where
+definition ord :: \<open>'a::ord tree \<Rightarrow> bool\<close> where
   \<open>ord \<equiv> rec_tree True (
     \<lambda> left' item' right' left_ord right_ord.
       left_ord \<and> right_ord \<and> (
         let cmp_item = \<lambda> cmp tree. \<forall> x \<in> tree_to_set tree. cmp item' x
         in cmp_item (\<ge>) left' \<and> cmp_item (\<le>) right'))\<close>
 
-fun ins :: \<open>'a::{linorder} \<Rightarrow> 'a tree \<Rightarrow> 'a tree\<close> where
+fun ins :: \<open>'a::ord \<Rightarrow> 'a tree \<Rightarrow> 'a tree\<close> where
   \<open>ins x Tip = Leaf x\<close> |
   \<open>ins x (Node left' val' right' =: tree) = (
     if x = val' then tree
@@ -40,6 +40,7 @@ lemma tree_to_set_ins_eq_insert_tree_to_set [simp]:
   by (induction tree) auto
 
 lemma tree_ordered_ins_of_ordered:
+  fixes tree :: \<open>'a::linorder tree\<close>
   assumes \<open>ord tree\<close>
   shows \<open>ord <| ins x tree\<close>
   using assms by (induction tree) (auto simp add: ord_def)
